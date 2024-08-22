d797dfa5948737e5d2021336a457b2aecaeb7a02
Ireneo James Membrere <40286207+ireneojmembrere@users.noreply.github.com>
2024-08-22 16:49:10 +0000

    streams example : planets

--- src/main/java/com/example/PlanetUtils.java
+++ src/main/java/com/example/PlanetUtils.java
@@ -5,19 +5,19 @@ import java.util.List;
 public class PlanetUtils {
 
   public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
-    return null;
+    return planets.stream().filter(planet -> planet.getName().startsWith("M")).toList();
   }
 
   public List<Planet> getPlanetsWithRings(List<Planet> planets) {
-    return null;
+    return planets.stream().filter(planet -> planet.getHasRings()).toList();
   }
 
   public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
-    return null;
+    return planets.stream().filter(planet -> planet.getNumberOfMoons() > 3).toList();
   }
 
   public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
-    return null;
+    return planets.stream().filter(planet -> planet.getDensity() > 5).toList();
   }
 
 }
